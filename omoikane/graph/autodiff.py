8d2fd0161be157e94757355df9250c54b4997548
SoloSynth1 <solosynth1@gmail.com>
2024-03-03 23:42:55 -0800

    add grad func for pow and cos

--- omoikane/graph/autodiff.py
+++ omoikane/graph/autodiff.py
@@ -13,7 +13,13 @@ reverse_mode_mapping = {
         if i == 0
         else -y.gradients * X[0].ndarray / X[1].ndarray / X[1].ndarray
     ),
+    "pow": (
+        lambda y, X, i: y.gradients * X[1].ndarray * (X[0].ndarray ** (-1 + X[1].ndarray))
+        if i == 0
+        else y.gradients * (X[0].ndarray ** X[1].ndarray) * np.log(X[0].ndarray)
+    ),
     "log": (lambda y, X, i: y.gradients / X[i].ndarray),
     "exp": (lambda y, X, i: y.gradients * np.exp(X[i].ndarray)),
     "sin": (lambda y, X, i: y.gradients * np.cos(X[i].ndarray)),
+    "cos": (lambda y, X, i: y.gradients * -np.sin(X[i].ndarray)),
 }
